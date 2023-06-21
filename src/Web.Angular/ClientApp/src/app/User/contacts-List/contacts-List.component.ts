85adb721cda26b9bcc34272852a6fa0796b1cd80
mbarkt3sto <mbark1236@gmail.com>
2023-06-21 11:00:12 +0100

    Refactor(Angular): Apply the search feature on 'contacts-List' Component

--- src/Web.Angular/ClientApp/src/app/User/contacts-List/contacts-List.component.ts
+++ src/Web.Angular/ClientApp/src/app/User/contacts-List/contacts-List.component.ts
@@ -1,22 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-import { Contact } from 'src/app/DTOs/Contact/Contact';
-import { ContactService } from 'src/app/Services/Contact/Contact.service';
+import { Contact }           from 'src/app/DTOs/Contact/Contact';
+import { ContactService }    from 'src/app/Services/Contact/Contact.service';
+import { StringUtilService } from 'src/app/Services/Helpers/Extensions/String/StringUtil.service';
 
 @Component({
-  selector: 'app-contacts-List',
+  selector   : 'app-contacts-List',
   templateUrl: './contacts-List.component.html',
-  styleUrls: ['./contacts-List.component.css']
+  styleUrls  : ['./contacts-List.component.css'],
 })
 export class ContactsListComponent implements OnInit {
+  originalContacts : Contact[] = [];
+  displayedContacts: Contact[] = [];
+  searchQuery      : string    = '';
 
-  contacts: Contact[] = [];
-
-  constructor(private contactService: ContactService) { }
+  constructor(
+    private contactService: ContactService,
+    private stringUtil    : StringUtilService
+  ) {}
 
   ngOnInit() {
     this.contactService.GetAll().subscribe((data) => {
-      this.contacts = data;
+      this.originalContacts  = data;
+      this.displayedContacts = data;
     });
   }
 
+  search() {
+    if (this.stringUtil.IsNullOrEmpty(this.searchQuery)) {
+      this.displayedContacts = this.originalContacts;
+    } else {
+      this.displayedContacts = this.originalContacts.filter(
+        (contact) =>
+          contact.name.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+          contact.email.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+          contact.phone.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    }
+  }
 }
